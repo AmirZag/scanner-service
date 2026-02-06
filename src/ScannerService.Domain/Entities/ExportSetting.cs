c81a32dc453f2fd06adf967b2ce0b7e80188bd85
Amirreza Qassemi <arezaqassemi@gmail.com>
2026-02-06 15:49:51 +0330

    Add project files.

---
+++ src/ScannerService.Domain/Entities/ExportSetting.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScannerService.Domain.Entities;
+
+internal class ExportSetting
+{
+    public int Id { get; set; }
+    public string OutputFormat { get; set; } = "PDF";
+    public string OutputDirectory { get; set; } = "";
+    public string FileName { get; set; } = "scan_{datetime}";
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+}
